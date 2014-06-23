9de1f8887aad666a1e6cba587363ab1ea33190b1
Brad Carmody <brad.carmody@hotmail>
2014-06-23 13:10:48 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,71 @@
+// for(var i=0; i<10; i++) {
+// 	console.log(i);
+// }
+
+
+// for(var i=0; i<=500; i = i+100) {
+// 	console.log(i);
+// }
+
+
+// for(var i=1; i<=64; i = i*2) {
+// 	console.log(i);
+// }
+
+
+// for(var i=1; i<=3; i = i+1) {
+// 	console.log(i);
+// 	console.log(i);
+// 	console.log(i);
+// }
+
+
+// Same result as above
+// for(var i=1; i<=3; i = i+1) {
+// 	for (var x=1; x<=3; x = x+1) {
+// 		console.log(i);
+// 	}
+// }
+
+// for(var i=0; i<=10; i = i+2) {
+// 	console.log(i);
+// }
+
+
+
+// increments of 3
+// for(var i=3; i<=15; i = i+3) {
+// 	console.log(i);
+// }
+
+
+// down down from 9 to 0 by 1
+// for (var i = 9; i>=0; i=i-1) {
+// 	console.log(i);
+// }
+
+// repeat pringing gruop "0 1 2 3" 3 times 
+
+// for (var i = 0; i <=3; i = i + 1) {
+// 	for (var x = 0; x <=3; x = x+1) {
+// 		console.log(x);
+// 	} 
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
